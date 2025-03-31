1cbf817a06818097ac88de7211e03b024b28db92
Bipul Kumar <133943874+bipul-eng@users.noreply.github.com>
2025-03-31 23:09:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./layout/Navbar";
+import Navbar from "./layout/navbar";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddUser from "./users/AddUser";
